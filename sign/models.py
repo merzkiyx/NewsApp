b2b3718ba6a5c01e14e8d8d9b5c2ee0ad73986b1
Anthony <136890969+merzkiyx@users.noreply.github.com>
2023-11-01 21:32:31 +0800

    Add files via upload

--- sign/models.py
+++ sign/models.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
-from allauth.account.forms import SignupForm
-from django.contrib.auth.models import Group
 
 
 class BaseRegisterForm(UserCreationForm):
@@ -18,12 +16,3 @@ class BaseRegisterForm(UserCreationForm):
                   "email",
                   "password1",
                   "password2", )
-
-
-class BasicSignupForm(SignupForm):
-
-    def save(self, request):
-        user = super(BasicSignupForm, self).save(request)
-        basic_group = Group.objects.get(name='common')
-        basic_group.user_set.add(user)
-        return user
--- sign/urls.py
+++ sign/urls.py
@@ -1,17 +1,15 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import BaseRegisterView
-from .views import upgrade_me
 
 urlpatterns = [
     path('login/',
-         LoginView.as_view(template_name = 'sign/login.html'),
+         LoginView.as_view(template_name = 'flatpages/login.html'),
          name='login'),
     path('logout/',
-         LogoutView.as_view(template_name = 'sign/logout.html'),
+         LogoutView.as_view(template_name = 'flatpages/logout.html'),
          name='logout'),
     path('signup/',
-         BaseRegisterView.as_view(template_name = 'sign/signup.html'),
+         BaseRegisterView.as_view(template_name = 'flatpages/signup.html'),
          name='signup'),
-    path('upgrade/', upgrade_me, name = 'upgrade')
 ]
