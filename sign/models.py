c12a7859cbcabc67bd7796224ae01d3ca7993f0d
Anthony <136890969+merzkiyx@users.noreply.github.com>
2023-11-08 19:48:04 +0800

    Add files via upload

--- sign/models.py
+++ sign/models.py
@@ -34,5 +34,5 @@ class IndexView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['is_not_premium'] = not self.request.user.groups.filter(name='author').exists()
+        context['is_not_authors'] = not self.request.user.groups.filter(name='authors').exists()
         return context
--- sign/views.py
+++ sign/views.py
@@ -14,7 +14,7 @@ class BaseRegisterView(CreateView):
 @login_required
 def upgrade_me(request):
     user = request.user
-    premium_group = Group.objects.get(name='premium')
-    if not request.user.groups.filter(name='premium').exists():
+    premium_group = Group.objects.get(name='authors')
+    if not request.user.groups.filter(name='authors').exists():
         premium_group.user_set.add(user)
-    return redirect('/')
+    return redirect('http://127.0.0.1:8000/post/')
